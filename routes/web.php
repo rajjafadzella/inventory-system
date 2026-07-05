80fa56b3f5d0bc7a34450ea02a7588d4864a5530
rajjafadzella <rajjafadzella@gmail.com>
2026-07-06 06:57:57 +0700

    Fix role

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', DashboardController::class)
-    ->middleware(['auth', 'verified', 'role:Admin,Manager'])
+    ->middleware(['auth', 'verified', 'role:Admin,Staff,Manager'])
     ->name('dashboard');
 
 Route::middleware('auth')->group(function () {
